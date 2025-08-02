404e45601a626982d0c6cd876f7b757133dd3dcb
Anoop Singh <anoopsinghssj3@gmail.com>
2025-08-02 09:13:00 +0530

    DF

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,10 @@ import React from 'react'
 const App = () => {
   return (
     <div>Hey this is Waste Reporter app
-      <p>Welcome to the Waste Reporter application! This is from nitin side
+      <p>Welcome to the Waste Reporter application! This is from nitin side</p>
       <p>Welcomeo the Waste Reporter application! This is from nitin side
       </p>
+      <div> anoop is here</div>
     </div>
   )
 }
