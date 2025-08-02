d89a8d1ff0f0047e3d50e5c770c5c22e43ba492e
dipendrakumarpatel <pateldipendra077@gmail.com>
2025-08-02 09:05:01 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>Hey this is Waste Reporter app
-      <p>Welcome to the Waste Reporter application! This is from nitin side
-      <p>Welcomeo the Waste Reporter application! This is from nitin side
-      </p>
-    </div>
+    <div>Hey this is Waste Reporter app</div>
   )
 }
 
